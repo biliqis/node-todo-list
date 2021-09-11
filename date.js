4b63c97122e63425aaecf021560a07730f4f8840
biliqis <biliqistemmy@gmail.com>
2021-09-11 10:58:32 -0400

    first commit

---
+++ date.js
@@ -0,0 +1,32 @@
+//jshint esvdersion:6
+ exports.getDate = function() {
+
+
+
+const today = new Date();
+
+    let options = {
+         weekday: 'long', 
+         year:    'numeric', 
+         month: 'long',
+         day: 'numeric' 
+        };
+
+    return today.toLocaleDateString("en-US", options)
+
+
+}
+
+exports.getDay = function () {
+
+
+
+ const today = new Date();
+
+    const options = {
+         weekday: 'long',  
+        };
+
+    return today.toLocaleDateString("en-US", options)
+}
+
